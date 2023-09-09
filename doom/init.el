8219b0a20e819728d648cf9bbf48e66283a6ce69
Akash Gopinath <ag1884@york.ac.uk>
2023-09-09 12:17:55 +0100

    Updated Doom emacs config

--- doom/init.el
+++ doom/init.el
@@ -150,11 +150,11 @@
         +journal             ; enable org journal
         +roam2               ; create a personal wiki 
         +pretty              ; replace asterisks with pretty org bullets
-        +publish +habit)            ; create static websites with org
+        +publish +habit +brain +dragndrop +jupyter)            ; create static websites with org
        php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python           ; beautiful is better than ugly
+       (python +lsp +cython +poetry +pyenv +pyright)           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
